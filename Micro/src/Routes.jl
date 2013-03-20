7a3f78e7c8f15ebf8fdc0e2cf2ed581b875ae3f0
Zachary Allaun <zach.allaun@gmail.com>
2013-03-20 15:48:50 -0400

    Remove accidentally committed stuffs

--- Micro/src/Routes.jl
+++ Micro/src/Routes.jl
@@ -7,15 +7,6 @@ function ismatch(v1::StringNode, v2::String)
 end
 ismatch(v1::String, v2::String) = v1 == v2
 
-get(app, "/users/<id>/edit") do req, res
-    data = db_req(req.resources[:id])
-    render(user_edit_page(data))
-end
-
-get(app, "/users/%user(?<id>\d+)"
-
-"/users/user254/edit"
-
 function searchroute(route)
     function searchpred(val)
         if ismatch(val, route[1])
