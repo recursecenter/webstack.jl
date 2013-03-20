f3377de556e716de70f80fee31d870fe81deaea3
Daniel Mendel <daniel@danielespeset.com>
2013-03-20 18:12:58 -0400

    Adds simple routing and working Micro example
    Introduces the concept of tree based routing tables.
    Routing can only handle "static" routes right now.
    Micro now leverages Meddle to build a middleware stack with itself at the end.

--- example-meddle.jl
+++ example-meddle.jl
@@ -8,6 +8,7 @@ for event in split("connect read write close error")
     http.events[event] = ((event) -> (client, args...) -> println(client.id,": $event"))(event)
 end
 http.events["error"] = (client, err) -> println(err)
+http.events["listen"] = (port)        -> println("Listening on $port...")
 
 server = Server(http)
 run(server, 8000)
