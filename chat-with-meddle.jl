7e12828b2ec5e17e8d523fc17aae5b22c4080549
Leah Hanson <astrieanna@gmail.com>
2013-03-22 01:21:48 -0400

    combine example-chat and example-meddle into chat-with-meddle, so that the fileserver can server the html file for the chat app. They share port 8080.

---
+++ chat-with-meddle.jl
@@ -0,0 +1,41 @@
+using Http
+using Meddle
+require("websockets.jl")
+
+##
+## Websockets stuff (port 8080)
+##
+
+#global Dict to store open connections in
+@show global connections = {0 => WebSocket(0,TcpSocket())}
+
+wsh = websocket_handler((req,client) -> begin
+  global connections
+  @show connections[client.id] = client 
+  while true
+    msg = read(client)
+    for (k,v) in connections
+      if k != client.id
+        write(v, msg)
+      end
+    end
+  end
+end)
+
+wshh = WebsocketHandler(wsh)
+
+##
+## File server stuff (port 8000)
+##
+
+stack = middleware(DefaultHeaders, CookieDecoder, FileServer(pwd()), NotFound)
+http = HttpHandler((req, res) -> Meddle.handle(stack, req, res))
+
+for event in split("connect read write close error")
+    http.events[event] = ((event) -> (client, args...) -> println(client.id,": $event"))(event)
+end
+http.events["error"] = (client, err) -> println(err)
+http.events["listen"] = (port)        -> println("Listening on $port...")
+
+server = Server(http,wshh)
+run(server, 8080)
