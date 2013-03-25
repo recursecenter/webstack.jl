103fda1431cec4e9b8f462a661b5b53930a05570
Zachary Allaun <zach.allaun@gmail.com>
2013-03-25 13:36:03 -0400

    Adds Httplib and decouples Websockets, Http, et al
    - Changes the request and client handling API to use new functions
      `handle` and `is_websocket_handshake` that dispatch on the handler.
    - Http exposes a WebsocketInterface abstract type with default
      implementations of these functions that throw errors.
    - Websockets implements these for a WebsocketHandler, which it defines.
    - In addition, generic stuff like Request, Response, Headers, et al.
      have been moved to an Httplib module.

--- examples/chat.jl
+++ examples/chat.jl
@@ -2,27 +2,25 @@ using Http
 using Websockets
 
 #global Dict to store open connections in
-@show global connections = {0 => WebSocket(0,TcpSocket())}
+@show global connections = {0 => Websocket(0,TcpSocket())}
 
-wsh = websocket_handler((req,client) -> begin
-  global connections
-  @show connections[client.id] = client 
-  while true
-    msg = read(client)
-    for (k,v) in connections
-      if k != client.id
-        write(v, msg)
-      end
+wsh = WebsocketHandler() do req, client
+    global connections
+    @show connections[client.id] = client
+    while true
+        msg = read(client)
+        for (k,v) in connections
+            if k != client.id
+                write(v, msg)
+            end
+        end
     end
-  end
-end)
-
-wshh = WebsocketHandler(wsh)
+end
 
 onepage = readall("./examples/chat-client.html")
 httph = HttpHandler() do req::Request, res::Response
   Response(onepage)
 end
 
-server = Server(httph,wshh)
-run(server,8080)
+server = Server(httph, wsh)
+run(server,8000)
