82cd6655f0cd92181d65dcd6d32bf79ff0e4adda
Daniel Mendel <daniel@danielespeset.com>
2013-03-28 17:21:02 -0400

    Ugly working chatroom

--- examples/chat.jl
+++ examples/chat.jl
@@ -3,22 +3,40 @@ using Websockets
 
 #global Dict to store open connections in
 global connections = Dict{Int,Websocket}()
+global usernames   = Dict{Int,String}()
+
+function decodeMessage( msg )
+    str = ""
+    for d in msg
+        str = string( str, convert(Char, d) )
+    end
+    str
+end
 
 wsh = WebsocketHandler() do req, client
     global connections
     @show connections[client.id] = client
+    @show usernames[client.id] = string(client.id)
     while true
         msg = read(client)
-        for (k,v) in connections
-            if k != client.id
-                write(v, msg)
+        msg = decodeMessage(msg)
+        if( length(msg) > 12 && msg[1:12] == "setusername:" )
+            println("SETTING USERNAME: $msg")
+            usernames[client.id] = msg[13:length(msg)]
+        end
+        if( length(msg) > 4 && msg[1:4] == "say:")
+            println("EMITTING MESSAGE: $msg")
+            for (k,v) in connections
+                if k != client.id
+                    write(v, usernames[client.id] * ": " * msg[5:length(msg)])
+                end
             end
         end
     end
 end
 
-onepage = readall("./examples/chat-client.html")
 httph = HttpHandler() do req::Request, res::Response
+  onepage = readall("./examples/chat-client.html")
   Response(onepage)
 end
 
