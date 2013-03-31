6dc19e89b0c55f322f7e660ce39ce5921810c449
Daniel Mendel <daniel@danielespeset.com>
2013-03-31 00:54:22 -0400

    Adds DynamicRoutes: regex, datatype, named & func

--- examples/micro.jl
+++ examples/micro.jl
@@ -24,5 +24,20 @@ get(app, "/regex/<test::%[0-9][0-9]-[a-z]*>") do req, res
 	"Now you have 2 problems! test: $(route_params(req, :test))"
 end
 
+get(app, "/datatype/<test::Int>") do req, res
+	"Int: $(route_params(req, :test))"
+end
+
+get(app, "/named/<test>") do req, res
+	"Named: $(route_params(req, :test))"
+end
+
+global boner = function( args... )
+	"Suck it."
+end
+
+get(app, "/converter/<test::boner>") do req, res
+	"Converter: $(route_params(req, :test))"
+end
 
 start(app, 8000)
